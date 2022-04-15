d2b15f829bd2f1b154b91fc8ad863198f1c9f4fd
Mona Lisa <Mona@Lisa>
2022-04-15 13:58:23 +0800

    Bug Fixed

---
+++ reacetest/precache-manifest.98401b2ec90088aad0561067185adcce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fbb2b4964988543db17",
+    "url": "/static/css/main.19e0b879.chunk.css"
+  },
+  {
+    "revision": "4fbb2b4964988543db17",
+    "url": "/static/js/main.402368b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87148942cbe2ee6453eb",
+    "url": "/static/js/2.c7eb75c5.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "94190be7c4441bf446701e98a0e7481a",
+    "url": "/index.html"
+  }
+];
