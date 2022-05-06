defb00b177fc998ebf19b4895d9c6fb65039e475
Ulukuga <ulu@ku.ga>
2022-05-06 16:04:40 +0800

    Bug Fixed

---
+++ info.js
@@ -0,0 +1,8 @@
+var state;
+    var createState = function() {
+        /* data from server is stored in 'state' object */
+        state = new ClientState(
+        "/helloprwp",
+        "U2FsdGVkX18asMwUOuubBbCruMhBtoaLivW4D1yHX2jOhZtSmpO7cr9Z9DU2lEnFXHy84Dbl7hDHCqDaytXOIIetKUjLa8jKMZLfJTiPKYhwiWZRRMdS+AqYWHgyGkxBEQ7o3t24tNmkb/wnd57v3aPOz9hHgOC2TR6BGZ0uuPETtSAUiS8nBUc2jBkWm/Yo1tvv1l1//YHhQXi1eSaPYw==",
+        false,2,1);
+    }
